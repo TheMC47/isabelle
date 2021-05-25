3edf24c65812807548330d6aeae0a4d401e404e2
Yecine Megdiche <yecine.megdiche@gmail.com>
2021-05-25 11:25:26 +0200

    Changed to a pretty text area, integrate new Lint_Report

--- src/Tools/jEdit/src/linter_dockable.scala
+++ src/Tools/jEdit/src/linter_dockable.scala
@@ -9,8 +9,12 @@ import linter._
 class Linter_Dockable(view: View, position: String)
     extends Dockable(view, position) {
 
-  private val text_area = new TextArea
-  set_content(new ScrollPane(text_area))
+  private var current_output: List[XML.Tree] = Nil
+
+  val output = new Pretty_Text_Area(view)
+  set_content(output)
+
+  override def detach_operation: Option[() => Unit] = output.detach_operation
 
   def lint(): Unit = {
 
@@ -21,31 +25,34 @@ class Linter_Dockable(view: View, position: String)
       if !snapshot.is_outdated
     } {
       val results = Linter.lint(snapshot, get_lints())
-      report_lint_results(results)
-    }
-  }
+      val content = results
+        .map(report(_))
+        .mkString("\n\n-------------------------------\n")
 
-  def report_lint_results(results: List[Linter.Lint_Report]): Unit = {
-    text_area.append(results.mkString("\n") + "\n\n\n")
-  }
-
-  def parse_current_command(): Unit = {
-
-    GUI_Thread.require {}
-
-    for {
-      snapshot <- PIDE.maybe_snapshot(view)
-      if !snapshot.is_outdated
-    } {
-      val command = PIDE.editor.current_command(view, snapshot) match {
-        case Some(command) => command
-        case None          => Command.empty
+      val text = XML.Text(content)
+      val new_output = Pretty.separate(text :: Nil)
+      if (current_output != new_output) {
+        output.update(
+          snapshot,
+          Command.Results.empty,
+          Pretty.separate(new_output)
+        )
+        current_output = new_output
       }
-      val parsed = Linter.parse_command(command)
-
-      text_area.append(s"$parsed" + "\n\n\n")
-
     }
+  }
+
+  def report(lint_report: Linter.Lint_Report): String = {
+    val result = new StringBuilder()
+    result ++= s"At ${lint_report.range.start.print}:   [${lint_report.lint_name}]\n\n"
+    result ++= lint_report.message
+    val edit = lint_report.edit match {
+      case None => ""
+      case Some((before, after)) =>
+        s"""\nConsider changing "$before" to "$after""""
+    }
+    result ++= edit
+    result.toString()
   }
 
   def lint_current_command(): Unit = {
@@ -56,29 +63,19 @@ class Linter_Dockable(view: View, position: String)
       snapshot <- PIDE.maybe_snapshot(view)
       if !snapshot.is_outdated
     } {
-      val command = PIDE.editor.current_command(view, snapshot) match {
-        case Some(command) => command
-        case None          => Command.empty
-      }
-      val lint_result = Linter.lint_command(command, get_lints())
-
-      text_area.append(s"$lint_result" + "\n\n\n")
+      val lint_result = for {
+        command <- PIDE.editor.current_command(view, snapshot)
+      } yield Linter.lint_command(command, snapshot, get_lints())
 
     }
   }
 
-  def get_lints(): List[Linter.Lint] =
-    List(
-      Linter.Implicit_Rule,
-      Linter.Unfinished_Proof,
-      Linter.Short_Name,
-      Linter.Print_Structure
-    )
+  def get_lints(): List[Linter.Lint] = Linter.all_lints
   /* main */
 
   private val main =
     Session.Consumer[Any](getClass.getName) { _ =>
-      GUI_Thread.later { lint_current_command() }
+      GUI_Thread.later { lint() }
     }
 
   override def init(): Unit = {
