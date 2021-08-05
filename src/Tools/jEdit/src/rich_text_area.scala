47fa584b3ec92bdea419111c47c31558f488df3b
Yecine Megdiche <yecine.megdiche@gmail.com>
2021-08-05 12:32:27 +0200

    Linter: squiggly lines

--- src/Tools/jEdit/src/rich_text_area.scala
+++ src/Tools/jEdit/src/rich_text_area.scala
@@ -379,12 +379,17 @@ class Rich_Text_Area(
             for {
               linter <- PIDE.plugin.linter.get
               Text.Info(range, severity) <- linter.lint_report(rendering.snapshot).ranges(line_range)
+
               r <- JEdit_Lib.gfx_range(text_area, range)
               color = rendering.lint_severity_color(severity)
             } {
               gfx.setColor(color)
-              val y0 = r.y + ((fm.getAscent + 4) min (line_height - 2))
-              gfx.drawLine(r.x, y0, r.x + r.length, y0)
+              val x0 = (r.x / 2) * 2
+              val y0 = r.y + fm.getAscent + 1
+              for (x1 <- Range(x0, x0 + r.length, 2)) {
+                val y1 = if (x1 % 4 < 2) y0 else y0 + 1
+                gfx.drawLine(x1, y1, x1 + 1, y1)
+              }
             }
           }
         }
